df9b8ec3e5d6e7264226207edc3b6e0f753992a8
laon2019 <laon5987@nate.com>
2023-05-05 12:53:23 +0900

    빈칸등록수정

--- src/main/resources/static/javascript/register.js
+++ src/main/resources/static/javascript/register.js
@@ -1,6 +1,16 @@
 function register() {
   let title = $(".text_title").val();
   let content = $(".text_content").val();
+
+  if (title === "" || content === "") {  // 제목 또는 내용이 비어있을 경우
+    if (title === "") {
+      $(".text_title").focus();  // 제목 필드에 포커스 이동
+    } else {
+      $(".text_content").focus();  // 내용 필드에 포커스 이동
+    }
+    return;  // 함수 종료
+  }
+
   let data = {
     title: title,
     content: content,
